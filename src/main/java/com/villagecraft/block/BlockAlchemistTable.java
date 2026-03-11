acb91ebff43fc5575363d95c9bf5eef5cdee150c
Chris McIntosh <cmcintosh@wembassy.com>
2026-03-11 15:32:45 -0500

    fix(blocks): Migrate all block classes to NeoForge 1.20.1 API
    Fixes applied to 24 block files:
    - BlockPlaceContext import: net.minecraft.world.item.context.BlockPlaceContext
    - ItemGroup → CreativeModeTab references (removed .tab() calls)
    - StateContainer → StateDefinition
    - InteractionResult/InteractionHand: moved to net.minecraft.world.InteractionResult/Hand
    - Material.WOOD → BlockBehaviour.Properties.of() (no arg)
    - .with(FACING) → .setValue(FACING)
    - stateDefinition() → stateDefinition
    - BlockRenderType → RenderShape (BlockChair)
    - Mirror/Rotation: net.minecraft.util → net.minecraft.world.level.block
    
    Blocks fixed:
    - BlockAlchemistTable
    - BlockAuctionHouse
    - BlockBardStand
    - BlockBar
    - BlockBeeKeepersHive
    - BlockBrawlerEquipment
    - BlockBuildersChest
    - BlockCaravanStop
    - BlockChair
    - BlockDraftingTable
    - BlockDrums
    - BlockEmbassy
    - BlockGuitarStand
    - BlockInn
    - BlockMicrophoneStand
    - BlockOreBox
    - BlockPotterWheel
    - BlockPyrotechnicTable
    - BlockSupplyOffice
    - BlockTitleOffice
    - BlockTownHall
    - BlockVillageCenter
    - BlockVillageManager
    - TradesmanHelmet

--- src/main/java/com/villagecraft/block/BlockAlchemistTable.java
+++ src/main/java/com/villagecraft/block/BlockAlchemistTable.java
@@ -5,49 +5,48 @@ import javax.annotation.Nullable;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.AbstractBlock.Properties;
-// Material removed - use BlockBehaviour
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.BlockItemUseContext;
-import net.minecraft.world.item.ItemGroup;
-import net.minecraft.state.DirectionProperty;
-import net.minecraft.state.StateContainer;
-import net.minecraft.state.properties.BlockStateProperties;
-import net.minecraft.util.ActionResultType;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.core.Direction;
-import net.minecraft.util.Hand;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.core.BlockPos;
-import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.level.Level;
 
 public class BlockAlchemistTable extends Block {
 	
-	public static Properties properties = Properties.create(Material.WOOD).hardnessAndResistance(3.5F).sound(SoundType.CLOTH).notSolid();
+	public static Properties properties = BlockBehaviour.Properties.of().strength(3.5F).sound(SoundType.CLOTH).noOcclusion();
 	public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
-	public static net.minecraft.item.Item.Properties item_properties = new net.minecraft.item.Item.Properties().group(ItemGroup.DECORATIONS).maxStackSize(64);
+	public static net.minecraft.world.item.Item.Properties item_properties = new net.minecraft.world.item.Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS).stacksTo(64);
 
 	public BlockAlchemistTable(Properties properties) {
 		super(properties);
-		setDefaultState(getStateContainer().getBaseState().with(FACING, Direction.NORTH));
+		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
 	}
 	
 	// Defines the properties needed for the blockstate
 	@Override
-	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) { 
-		super.fillStateContainer(builder);
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) { 
+		super.createBlockStateDefinition(builder);
 		builder.add(FACING);
 	}
 	
 	@Nullable
     @Override
-    public BlockState getStateForPlacement(BlockItemUseContext context) {
-        return getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
     }
 	
 	// When activated we will have the player sit
 	// @TODO: learn how to do this
 	@Override
-	public ActionResultType onBlockActivated(BlockState state, Level worldIn, BlockPos pos, Player player, Hand handIn, BlockRayTraceResult blockRayTraceResult) { 
-		return ActionResultType.SUCCESS;
+	public InteractionResult use(BlockState state, Level worldIn, BlockPos pos, Player player, InteractionHand handIn, BlockHitResult blockRayTraceResult) { 
+		return InteractionResult.SUCCESS;
 	}
 }
