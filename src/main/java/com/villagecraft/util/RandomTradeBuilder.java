1ccb8a220d1f6f60a585e4deb73e15396b1159a5
Chris McIntosh <cmcintosh@wembassy.com>
2026-03-11 18:45:09 -0500

    Fix TradeTypes.java and RandomTradeBuilder.java: ITrade -> ItemListing

--- src/main/java/com/villagecraft/util/RandomTradeBuilder.java
+++ src/main/java/com/villagecraft/util/RandomTradeBuilder.java
@@ -6,7 +6,7 @@ import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
-import net.minecraft.world.entity.npc.VillagerTrades.ITrade;
+import net.minecraft.world.entity.npc.VillagerTrades.ItemListing;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -173,7 +173,7 @@ public class RandomTradeBuilder
         return this.price != null && this.forSale != null;
     }
     
-    public ITrade build()
+    public ItemListing build()
     {
         return (entity, random) -> !this.canBuild() ? null : new MerchantOffer(this.price.apply(random), this.price2.apply(random), this.forSale.apply(random), this.maxTrades, this.xp, this.priceMult);
     }
--- src/main/java/com/villagecraft/util/TradeTypes.java
+++ src/main/java/com/villagecraft/util/TradeTypes.java
@@ -3,6 +3,7 @@ package com.villagecraft.util;
 import java.lang.reflect.Constructor;
 
 import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.entity.npc.VillagerTrades.ItemListing;
 import net.minecraft.world.item.Item;
 
 public class TradeTypes {
@@ -10,18 +11,18 @@ public class TradeTypes {
 	  
 	private static Constructor<?> ctr2;
 	  
-	public static VillagerTrades.ITrade EmeraldForItemsTrade(Item item, int count, int maxUses, int xpValue) {
-	    VillagerTrades.ITrade ret = null;
+	public static ItemListing EmeraldForItemsTrade(Item item, int count, int maxUses, int xpValue) {
+	    ItemListing ret = null;
 	    try {
-	      ret = (VillagerTrades.ITrade)ctr1.newInstance(new Object[] { item, Integer.valueOf(count), Integer.valueOf(maxUses), Integer.valueOf(xpValue) });
+	      ret = (ItemListing)ctr1.newInstance(new Object[] { item, Integer.valueOf(count), Integer.valueOf(maxUses), Integer.valueOf(xpValue) });
 	    } catch (Exception exception) {}
 	    return ret;
 	  }
 	  
-   public static VillagerTrades.ITrade ItemsForEmeraldsTrade(Item item, int cost, int count, int xpValue) {
-     VillagerTrades.ITrade ret = null;
+   public static ItemListing ItemsForEmeraldsTrade(Item item, int cost, int count, int xpValue) {
+     ItemListing ret = null;
      try {
-       ret = (VillagerTrades.ITrade)ctr2.newInstance(new Object[] { item, Integer.valueOf(cost), Integer.valueOf(count), Integer.valueOf(xpValue) });
+       ret = (ItemListing)ctr2.newInstance(new Object[] { item, Integer.valueOf(cost), Integer.valueOf(count), Integer.valueOf(xpValue) });
      } catch (Exception exception) {}
      return ret;
    }
