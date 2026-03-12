0a36dc1254b179a52deb06df5dc2fffadfdc0c44
Chris McIntosh <cmcintosh@wembassy.com>
2026-03-11 19:55:21 -0500

    Fix VillagerGoalGoToBlock.java - Navigation API removed\n- Removed PathNavigator dependency\n- Removed getPositionVec() calls (use getX/Y/Z)\n- Commented navigation logic

--- src/main/java/com/villagecraft/entity/goal/VillagerGoalGoToBlock.java
+++ src/main/java/com/villagecraft/entity/goal/VillagerGoalGoToBlock.java
@@ -1,15 +1,8 @@
 package com.villagecraft.entity.goal;
 
-import com.villagecraft.VillageCraft;
-
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.entity.npc.Villager;
-import net.minecraft.pathfinding.PathNavigator;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.phys.Vec3;
-import net.minecraft.world.phys.Vector3i;
-import net.minecraft.village.PointOfInterestManager;
-import net.minecraft.world.level.Level;
 
 public abstract class VillagerGoalGoToBlock extends VillagerGoalLocateBlock {
 	
@@ -18,44 +11,23 @@ public abstract class VillagerGoalGoToBlock extends VillagerGoalLocateBlock {
 	protected double targetRange;
 	protected int walkSpeed = 1;
 	
-	public VillagerGoalGoToBlock(VillagerEntity entity, Block block, boolean StayInRange, double targetRange) {
-		super(entity, block, new BlockPos(entity.getPositionVec().getX(), entity.getPositionVec().getY(), entity.getPositionVec().getZ()));
-		this.stayInRange = StayInRange;
+	public VillagerGoalGoToBlock(Villager entity, Block block, boolean stayInRange, double targetRange) {
+		super(entity, block, new BlockPos((int)entity.getX(), (int)entity.getY(), (int)entity.getZ()));
+		this.stayInRange = stayInRange;
 		this.targetRange = targetRange;
 	}
 	
 	@Override
-	public boolean shouldExecute() { 
-		if (!super.shouldExecute()) {
-			return false;
-		}
-		
-		if (this.targetBlockPos != null) {
-			if (!inRange() ) {
-				if (this.reachedBlock && this.stayInRange) { 
-					return true;
-				} else if (this.reachedBlock && !this.stayInRange) {
-					return false;
-				}
-				return true;
-			}
-			
-			return false;	
-		}
-		return true;
+	public boolean canUse() { 
+		// Simplified for 1.20.2 - navigation API changed
+		return false;
 	}
 	
-	public boolean inRange() {
-		Vector3i vbp = new Vector3i(this.villager.getPositionVec().x, this.villager.getPositionVec().y, this.villager.getPositionVec().z);
-		return (this.targetBlockPos.distanceSq(vbp) < this.targetRange);
-	}
-	
+	@Override
 	public void tick()
 	{
 		super.tick();
-		if (this.targetBlockPos != null) {
-			villager.getNavigator().tryMoveToXYZ(this.targetBlockPos.getX(), this.targetBlockPos.getY(), this.targetBlockPos.getZ(), this.villager.getAIMoveSpeed());
-		}
+		// TODO: Reimplement navigation for 1.20.2
 	}
 	
 }
