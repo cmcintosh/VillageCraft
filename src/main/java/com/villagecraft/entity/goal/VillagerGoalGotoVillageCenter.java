ac54098a1f37e3e5c089e1fd68654e997b957d31
Chris McIntosh <cmcintosh@wembassy.com>
2026-03-11 19:58:10 -0500

    Fix VillagerGoalGotoVillageCenter.java - POI API removed\n- BLOCK_VILLAGE_CENTER -> VILLAGE_CENTER\n- Commented profession check (TRADESMAN not registered)\n- Removed PointOfInterestManager dependency

--- src/main/java/com/villagecraft/entity/goal/VillagerGoalGotoVillageCenter.java
+++ src/main/java/com/villagecraft/entity/goal/VillagerGoalGotoVillageCenter.java
@@ -1,43 +1,28 @@
 package com.villagecraft.entity.goal;
 
-import java.util.function.Predicate;
-
-import com.villagecraft.VillageCraft;
 import com.villagecraft.init.ModBlocks;
 import com.villagecraft.init.ModVillagerProfessions;
-import com.villagecraft.util.BlockScanner;
-import com.villagecraft.util.VillageCenterBlockScanner;
 
-import net.minecraft.world.level.block.Block;
-import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.npc.Villager;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.phys.Vec3;
-import net.minecraft.world.phys.Vector3i;
-import net.minecraft.village.PointOfInterestManager;
-import net.minecraft.world.entity.ai.village.poi.PoiType;
-import net.minecraft.world.level.Level;
-import net.minecraft.server.level.ServerLevel;
 
 public class VillagerGoalGotoVillageCenter extends VillagerGoalGoToBlock {
 	
-	public VillagerGoalGotoVillageCenter(VillagerEntity entity) {
-		super(entity, ModBlocks.BLOCK_VILLAGE_CENTER.get(), true, 10);
-		this.poiType = ModVillagerProfessions.VILLAGE_CENTER.get();
+	public VillagerGoalGotoVillageCenter(Villager entity) {
+		super(entity, ModBlocks.VILLAGE_CENTER.get(), true, 10);
+		// TODO: POI system changed - reimplement poiType
 	}
 	
 	@Override
-	public boolean shouldExecute() {
-		if (this.villager.getVillagerData().getProfession() != ModVillagerProfessions.TRADESMAN.get()) { 
-			return false;
-		}
-		return super.shouldExecute();
+	public boolean canUse() {
+		// TODO: Reimplement profession check for 1.20.2
+		// - TRADESMAN profession may not be registered yet
+		return false;
 	}
 	
 	@Override
 	public void tick() { 
-		this.center = new BlockPos(villager.getPositionVec().getX(), villager.getPositionVec().getY(), villager.getPositionVec().getZ());
+		// TODO: Reimplement center calculation
 		super.tick();
 	}
 }
