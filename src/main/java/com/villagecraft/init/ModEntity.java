aad09a6f6e920357ce20a0d29c42ee8467c9338e
Spock <spock@wembassy.co>
2026-03-11 21:46:11 -0500

    feat: Add VillageCraftVillager entity registration

--- src/main/java/com/villagecraft/init/ModEntity.java
+++ src/main/java/com/villagecraft/init/ModEntity.java
@@ -1,10 +1,12 @@
 package com.villagecraft.init;
 
+import com.villagecraft.entity.VillageCraftVillager;
 import com.villagecraft.entity.vanilla.IronGolem;
 import com.villagecraft.util.Reference;
 
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.registries.Registries;
 import net.neoforged.neoforge.registries.DeferredHolder;
@@ -20,5 +22,15 @@ public class ModEntity {
 					.sized(1.4F, 2.7F)
 					.clientTrackingRange(10)
 					.build(new ResourceLocation(Reference.MODID, "iron_golem").toString()));
+
+	// VillageCraftVillager entity registration
+	public static final DeferredHolder<EntityType<?>, EntityType<VillageCraftVillager>> VILLAGECRAFT_VILLAGER = ENTITY_TYPES
+			.register("villagecraft_villager",
+					() -> EntityType.Builder.<VillageCraftVillager>of((EntityType<VillageCraftVillager> type, net.minecraft.world.level.Level level) -> {
+									return new VillageCraftVillager(type, level, net.minecraft.world.entity.npc.VillagerType.PLAINS);
+								}, MobCategory.MISC)
+					.sized(0.6F, 1.95F)
+					.clientTrackingRange(10)
+					.build(new ResourceLocation(Reference.MODID, "villagecraft_villager").toString()));
 
 }
