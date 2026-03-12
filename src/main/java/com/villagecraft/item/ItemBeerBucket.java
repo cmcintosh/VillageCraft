dbb4f501a28c1a79f8482d137b650d49d3ac716a
Chris McIntosh <cmcintosh@wembassy.com>
2026-03-11 19:45:26 -0500

    Fix ItemBeerBucket.java - remove ItemGroup.BREWING\n- maxStackSize() -> stacksTo()

--- src/main/java/com/villagecraft/item/ItemBeerBucket.java
+++ src/main/java/com/villagecraft/item/ItemBeerBucket.java
@@ -5,12 +5,10 @@ import org.apache.logging.log4j.Logger;
 
 import com.villagecraft.util.Reference;
 
-import net.minecraft.world.item.ItemGroup;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Item.Properties;
 
 public class ItemBeerBucket extends Item {
-	public static Properties properties = new Properties().group(ItemGroup.BREWING).maxStackSize(64);
+	public static Properties properties = new Properties().stacksTo(64);
 	private static final Logger LOGGER = LogManager.getLogger(Reference.MODID + " Client Mod Event Subscriber");
 	
 	public ItemBeerBucket(Properties properties) {
