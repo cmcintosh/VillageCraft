f7dca6760660148e300e30cb4d72d0848bbcc69c
Chris McIntosh <cmcintosh@wembassy.com>
2026-03-11 19:46:58 -0500

    Fix IronGolem.java - remove problematic overrides\n- dropCustomDeathLoot signature changed\n- getDismountLocationForPassenger signature changed\n- shouldDropExperience signature changed

--- src/main/java/com/villagecraft/entity/vanilla/IronGolem.java
+++ src/main/java/com/villagecraft/entity/vanilla/IronGolem.java
@@ -1,33 +1,23 @@
 package com.villagecraft.entity.vanilla;
 
+import net.minecraft.server.level.ServerLevel;
+
 import javax.annotation.Nullable;
 
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Saddleable;
 import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.attributes.DefaultAttributes;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.network.syncher.EntityDataAccessor;
-import net.minecraft.network.syncher.EntityDataSerializers;
-import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.level.Level;
 
 /**
  * Custom Iron Golem entity for VillageCraft.
- * Uses 1.20.1 compatible API.
+ * TODO: Complete 1.20.2 migration
  */
 public class IronGolem extends net.minecraft.world.entity.animal.IronGolem implements Saddleable {
-	
-    private static final Ingredient TEMPTATION_ITEMS = Ingredient.of(Items.CARROT, Items.POTATO, Items.BEETROOT);
 	
 	public IronGolem(EntityType<? extends net.minecraft.world.entity.animal.IronGolem> type, Level worldIn) {
 		super(type, worldIn);
@@ -41,45 +31,29 @@ public class IronGolem extends net.minecraft.world.entity.animal.IronGolem imple
             .add(Attributes.ATTACK_DAMAGE, 15.0D);
     }
 	
-	@Override
-	protected void dropCustomDeathLoot() {
-		super.dropCustomDeathLoot();
-	}
+	// TODO: Reimplement overrides for 1.20.2 - dropCustomDeathLoot signature changed
+	// TODO: Reimplement getDismountLocationForPassenger - signature changed
+	// TODO: Reimplement shouldDropExperience - signature changed
 	
-	protected boolean shouldDropExperience() {
-		return true;
-	}
-
 	@Nullable
 	@Override
-	public AgeableMob getBreedOffspring() {
+	public AgeableMob getBreedOffspring(ServerLevel level, AgeableMob otherParent) {
 		return null;
 	}
-
+	
+	@Override
+	public boolean isSaddleable() {
+		return false;
+	}
+	
+	@Override
+	public void equipSaddle(@Nullable SoundSource source) {
+		// Saddle implementation
+	}
+	
 	@Override
 	public boolean isSaddled() {
 		return false;
 	}
 
-	@Override
-	public boolean isSaddleable() {
-		return false;
-	}
-
-	@Override
-	public void equipSaddle(SoundSource soundSource) {
-		
-	}
-
-	/**
-	 * For vehicles, the first passenger is generally considered the controller and "drives" the vehicle.
-	 */
-	@Nullable
-	public Player getControllingPassenger() {
-		return this.getPassengers().isEmpty() ? null : (Player) this.getPassengers().get(0);
-	}
-	
-	public Vec3 getDismountLocationForPassenger() {
-		return super.getDismountLocationForPassenger();
-	}
 }
