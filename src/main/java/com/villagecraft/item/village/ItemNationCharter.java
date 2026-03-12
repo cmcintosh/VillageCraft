25bf0bdb9a61c887744b8da9608e8faafdcb70e1
Chris McIntosh <cmcintosh@wembassy.com>
2026-03-11 19:45:36 -0500

    Fix ItemNationCharter.java - remove ItemGroup.MISC\n- maxStackSize() -> stacksTo()\n- Added proper imports

--- src/main/java/com/villagecraft/item/village/ItemNationCharter.java
+++ src/main/java/com/villagecraft/item/village/ItemNationCharter.java
@@ -1,7 +1,5 @@
 package com.villagecraft.item.village;
 
-import java.util.List;
-
 import javax.annotation.Nullable;
 
 import org.apache.logging.log4j.LogManager;
@@ -9,27 +7,27 @@ import org.apache.logging.log4j.Logger;
 
 import com.villagecraft.util.Reference;
 
-import net.minecraft.client.util.TooltipFlag;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemGroup;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Item.Properties;
-import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.common.util.NonNullSupplier;
 
+import java.util.List;
+import net.minecraft.network.chat.Component;
 
 public class ItemNationCharter extends Item {
 	
-	public static Properties properties = new Properties().group(ItemGroup.MISC).maxStackSize(1);
+	public static Properties properties = new Properties().stacksTo(1);
 	private static final Logger LOGGER = LogManager.getLogger(Reference.MODID + " Client Mod Event Subscriber");
 	
 	public ItemNationCharter(Properties properties) {
 		super(properties);
 	}
 	
-	
-	
+	@Override
+	public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltipComponents, TooltipFlag isAdvanced) {
+		super.appendHoverText(stack, level, tooltipComponents, isAdvanced);
+	}
+
 }
