1cc895eb700722ce126cd9bdf406aff901feca5c
Chris McIntosh <cmcintosh@wembassy.com>
2026-03-12 11:11:17 -0500

    feat: Fix ModContainer MenuType registration for NeoForge 1.20.2
    - Use IMenuTypeExtension.create() for MenuType registration
    - Use IContainerFactory for proper factory pattern
    - VillageCenterContainer GUI now properly registered
    - Removed TODO - functionality complete
    - BUILD SUCCESSFUL

--- src/main/java/com/villagecraft/init/ModContainer.java
+++ src/main/java/com/villagecraft/init/ModContainer.java
@@ -10,16 +10,32 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.common.extensions.IMenuTypeExtension;
 
+/**
+ * Container registration for VillageCraft.
+ * Handles MenuType registration for GUIs.
+ * Updated for NeoForge 1.20.2
+ */
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModContainer {
 	
 	public static int VILLAGE_CENTER_GUI_ID = 0;
 	
-	public static final DeferredRegister<MenuType<?>> CONTAINER_TYPE = DeferredRegister.create(Registries.MENU, Reference.MODID);
+	public static final DeferredRegister<MenuType<?>> CONTAINER_TYPE = 
+		DeferredRegister.create(Registries.MENU, Reference.MODID);
 	
-	// TODO: 1.20.2 MenuType constructor is protected
-	// Need to register differently in NeoForge
-	public static final DeferredHolder<MenuType<?>, MenuType<VillageCenterContainer>> VILLAGE_CENTER_CONTAINER = null;
-
+	/**
+	 * VillageCenter Container registration.
+	 * NeoForge 1.20.2: Use IMenuTypeExtension.create() with factory
+	 */
+	public static final DeferredHolder<MenuType<?>, MenuType<VillageCenterContainer>> VILLAGE_CENTER_CONTAINER = 
+		CONTAINER_TYPE.register(
+			"village_center",
+			() -> IMenuTypeExtension.create(
+				(net.neoforged.neoforge.network.IContainerFactory<VillageCenterContainer>) (windowId, inv, data) -> 
+					new VillageCenterContainer(windowId, inv, data)
+			)
+		);
+	
 }
