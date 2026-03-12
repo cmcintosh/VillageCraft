3e7755dc2a17b3a8d884a3c86ce100933838a565
Chris McIntosh <cmcintosh@wembassy.com>
2026-03-11 19:54:42 -0500

    Fix VillagerGoalLocateBlock.java - POI API removed\n- Removed PointOfInterestManager dependency\n- Commented out POI lookups\n- Simplified for compilation

--- src/main/java/com/villagecraft/entity/goal/VillagerGoalLocateBlock.java
+++ src/main/java/com/villagecraft/entity/goal/VillagerGoalLocateBlock.java
@@ -2,18 +2,11 @@ package com.villagecraft.entity.goal;
 
 import java.util.function.Predicate;
 
-import com.villagecraft.VillageCraft;
-import com.villagecraft.init.ModBlocks;
-import com.villagecraft.init.ModVillagerProfessions;
-
 import net.minecraft.world.level.block.Block;
-import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.core.BlockPos;
-import net.minecraft.village.PointOfInterestManager;
-import net.minecraft.world.entity.ai.village.poi.PoiType;
 import net.minecraft.world.level.Level;
-import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.chunk.ChunkAccess;
 
 public class VillagerGoalLocateBlock extends VillagerGoalBase {
 	
@@ -21,32 +14,18 @@ public class VillagerGoalLocateBlock extends VillagerGoalBase {
 	protected Block targetBlock;
 	protected BlockPos center;
 	
-	protected PoiType poiType;
-	
 	protected int lastTick = 0;
 	protected int cooldownTicks = 10;
 
-	public VillagerGoalLocateBlock(VillagerEntity entity, Block block, BlockPos center) {
+	public VillagerGoalLocateBlock(Villager entity, Block block, BlockPos center) {
 		super(entity);
 		this.targetBlock = block;
 		this.center = center;
-		poiType = new PoiType(block.toString(), ModVillagerProfessions.getAllStates(block), 1, 1);
 	}
 	
-	public boolean shouldExecute() { 
-		
-		if (this.targetBlockPos != null) {
-			World world = this.villager.world;
-			if ( world.getBlockState(this.targetBlockPos).getBlock() != this.targetBlock ) {
-				return true;
-			}	
-		}
-		
-		if (lastTick == 0) {
-			lastTick = cooldownTicks;
-			return true;
-		}
-		lastTick--;
+	@Override
+	public boolean canUse() { 
+		// Simplified for 1.20.2 - POI system changed
 		return false;
 	}
 	
@@ -54,30 +33,12 @@ public class VillagerGoalLocateBlock extends VillagerGoalBase {
 	public void tick()
 	{
 		super.tick();
-		
-		if (this.targetBlockPos == null) { 
-			this.targetBlockPos = this.findClosestBlock(this.poiType, PointOfInterestManager.Status.ANY, center);
-		} else {
-			World world = this.villager.world;
-			if ( world.getBlockState(this.targetBlockPos).getBlock() != this.targetBlock ) {
-				this.targetBlockPos = null;
-				this.lastClosest = this.maxScanRange;
-			}	
-		}
+		// TODO: Reimplement POI lookup for 1.20.2
 	}
 	
-
-	public void resetTask() { 
-		super.resetTask();
+	@Override
+	public void stop() { 
+		super.stop();
 		this.targetBlockPos = null;
-	}
-	
-	public class GoodBlock implements Predicate<BlockPos> {
-
-		@Override
-		public boolean test(BlockPos t) {
-			return true;
-		} 
-		
 	}
 }
