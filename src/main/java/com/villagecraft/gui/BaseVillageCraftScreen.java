c08aa38f3964041b9f5a8b44920d3de7a641e479
Chris McIntosh <cmcintosh@wembassy.com>
2026-03-11 20:10:14 -0500

    Fix BaseVillageCraftScreen.java - GUI API changed\n- PoseStack -> GuiGraphics\n- Simplified base class

--- src/main/java/com/villagecraft/gui/BaseVillageCraftScreen.java
+++ src/main/java/com/villagecraft/gui/BaseVillageCraftScreen.java
@@ -1,58 +1,20 @@
 package com.villagecraft.gui;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.systems.RenderSystem;
-
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.chat.Component;
 
+// TODO: Reimplement base screen for 1.20.2
 public class BaseVillageCraftScreen<T extends AbstractContainerMenu> extends AbstractContainerScreen<T> {
 	
-	protected T container;
-	protected Inventory playerInventory;
-	
-	// Backgrounds
-	protected static ResourceLocation TEXTURE = new ResourceLocation("vcm:textures/gui/container/village_info.png");
-	protected static final ResourceLocation TEXTURE_BACKDROP = new ResourceLocation("vcm:textures/gui/container/inventory_backdrop.png");
-	
-	// Initial Positions
-	private float mousePosx;
-    private float mousePosY;
-    
-	protected int StartX = 20;
-	protected int StartY = 40;
-	
-	// Padding for gui
-	protected int PaddingX= 20;
-	protected int PaddingY = 20;
-
-	
-	public BaseVillageCraftScreen(T screenContainer, Inventory inv, Component titleIn) {
-		super(screenContainer, inv, titleIn);
-		this.container = screenContainer;
-		this.playerInventory = inv;
+	public BaseVillageCraftScreen(T container, Inventory inv, Component title) {
+		super(container, inv, title);
 	}
 
 	@Override
-	public void render(PoseStack matrixStack, int mouseX, int mouseY, float partialTicks) {
-		this.renderBackground(matrixStack);
-		super.render(matrixStack, mouseX, mouseY, partialTicks);
+	protected void renderBg(GuiGraphics graphics, float partialTick, int mouseX, int mouseY) {
+		// TODO: Reimplement for 1.20.2
 	}
-	
-	@Override
-	protected void renderBg(PoseStack matrixStack, float partialTicks, int x, int y) {
-		this.renderBackground(matrixStack);
-		this.mousePosx = x;
-        this.mousePosY = y;
-        int k = (this.width - this.imageWidth) / 2;
-        int l = (this.height - this.imageHeight) / 2;
-        RenderSystem.setShaderTexture(0, TEXTURE_BACKDROP);
-        this.blit(matrixStack, k, l, 0, 0, this.imageWidth, this.imageHeight);
-        RenderSystem.setShaderTexture(0, TEXTURE);
-        this.blit(matrixStack, k, l, 0, 0, this.imageWidth, this.imageHeight);
-	}
-	
 }
