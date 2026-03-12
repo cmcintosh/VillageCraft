c838deef699de2c98bf678b631ef11582cd9e93a
Chris McIntosh <cmcintosh@wembassy.com>
2026-03-11 19:45:12 -0500

    Fix ItemHeart.java - remove ItemGroup.MISC\n- maxStackSize() -> stacksTo()

--- src/main/java/com/villagecraft/item/ItemHeart.java
+++ src/main/java/com/villagecraft/item/ItemHeart.java
@@ -5,13 +5,11 @@ import org.apache.logging.log4j.Logger;
 
 import com.villagecraft.util.Reference;
 
-import net.minecraft.world.item.ItemGroup;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Item.Properties;
 
 public class ItemHeart extends Item {
 	
-	public static Properties properties = new Properties().group(ItemGroup.MISC).maxStackSize(64);
+	public static Properties properties = new Properties().stacksTo(64);
 	private static final Logger LOGGER = LogManager.getLogger(Reference.MODID + " Client Mod Event Subscriber");
 	
 	public ItemHeart(Properties properties) {
